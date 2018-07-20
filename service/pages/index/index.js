89aaad509d1a0b8fcae3d147fa9cb7edddee1de9
Your Name <1275674530@qq.com>
2018-07-20 17:42:10 +0800

    checkin Carservice

---
+++ service/pages/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    loadingHidden: false , // loading
+    userInfo: {},
+    swiperCurrent: 0,  
+    selectCurrent:0,
+    categories: [],
+    activeCategoryId: 0,
+    goods:[],
+    scrollTop:0,
+    loadingMoreHidden:true,
+
+    hasNoCoupons:true,
+    coupons: [],
+    searchInput: '',
+  },
+  //事件处理函数
+  swiperchange: function(e) {
+      //console.log(e.detail.current)
+       this.setData({  
+        swiperCurrent: e.detail.current  
+    })  
+  },
+  toDetailsTap:function(e){
+    wx.navigateTo({
+      url:"/pages/goods-details/index?id="+e.currentTarget.dataset.id
+    })
+  },
+  bindTypeTap: function(e) {
+     this.setData({  
+        selectCurrent: e.index  
+    })  
+  },
+  onLoad: function () {
+    var that = this
+    var imageInfo = [{
+      picUrl: "../../images/banner1.jpg"
+    },
+      {
+        picUrl: "../../images/banner2.jpg"
+      }];
+    that.setData({
+      banners: imageInfo
+    });
+  },
+  onPageScroll(e) {
+    let scrollTop = this.data.scrollTop
+    this.setData({
+      scrollTop: e.scrollTop
+    })
+   },
+  onShareAppMessage: function () {
+    return {
+      title: wx.getStorageSync('mallName') + '——' + app.globalData.shareProfile,
+      path: '/pages/index/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }
+})
